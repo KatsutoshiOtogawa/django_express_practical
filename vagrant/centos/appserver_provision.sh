b240ceeea1f17a39d3204f5457853b646d86ee1e
KatsutoshiOtogawa <gh1ab7im@gmail.com>
2020-09-25 17:14:33 +0900

    upate

--- vagrant/centos/appserver_provision.sh
+++ vagrant/centos/appserver_provision.sh
@@ -8,14 +8,26 @@ pip3 install pipenv
 
 # expressのためnodejsインストール
 # nodejs12はltsのバージョン
+# [nexe](https://github.com/nexe/nexe)
 curl --silent --location https://rpm.nodesource.com/setup_12.x | bash -
 curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
 yum install -y yarn
+yarn global add nexe
+
 
 # postgresql クライアントをインストール
 # postgresql サーバーとバージョンがあっている必要がある。
 yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 yum install -y postgresql12
+
+# yum searchするときレポジトリが404だとエラーになるので、レポジトリを無効にしておく。
+yum-config-manager --disable pgdg-common
+yum-config-manager --disable pgdg10
+yum-config-manager --disable pgdg11
+yum-config-manager --disable pgdg12
+yum-config-manager --disable pgdg13
+yum-config-manager --disable pgdg95
+yum-config-manager --disable pgdg96
 
 # nginxサーバーインストール
 yum install -y epel-release
@@ -44,3 +56,43 @@ firewall-cmd --add-service=https --zone=public --permanent
 # nginxのリバースプロキシを使う場合に必要なselinuxの設定
 setsebool -P httpd_can_network_connect on
 setsebool -P httpd_can_network_relay on
+
+# アプリケーションをサービスとして登録
+# 本番字はnexeでまとめた物を実行
+
+cat << END > /etc/systemd/system/site.service
+[Unit]
+Description = site provide daemon
+After=syslog.target network.target
+
+[Service]
+ExecStart = /usr/bin/node /home/$APP_USER/site/app.js
+WorkingDirectory=/home/$APP_USER/site
+KillMode=process
+Restart = always
+Type = simple
+User=$APP_USER
+Group=$APP_GROUP
+
+[Install]
+WantedBy = multi-user.target
+END
+
+
+cat << END > /etc/systemd/system/input.service
+[Unit]
+Description = input provide daemon
+After=syslog.target network.target
+
+[Service]
+ExecStart = /usr/bin/node /home/$APP_USER/input/app.js
+WorkingDirectory=/home/$APP_USER/input
+KillMode=process
+Restart = always
+Type = simple
+User=$APP_USER
+Group=$APP_GROUP
+
+[Install]
+WantedBy = multi-user.target
+END
--- vagrant/centos/dbserver_provision.sh
+++ vagrant/centos/dbserver_provision.sh
@@ -5,6 +5,16 @@ yum update -y
 yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 yum install -y postgresql12-server
 /usr/pgsql-12/bin/postgresql-12-setup initdb
+
+# レポジトリを無効にしておく。
+yum-config-manager --disable pgdg-common
+yum-config-manager --disable pgdg10
+yum-config-manager --disable pgdg11
+yum-config-manager --disable pgdg12
+yum-config-manager --disable pgdg13
+yum-config-manager --disable pgdg95
+yum-config-manager --disable pgdg96
+
 # su - postgres -c "initdb -D /var/lib/pgsql/data -E UTF8 --no-locale"
 systemctl enable postgresql-12
 systemctl start postgresql-12
